073439ae729d73b880ec6d4b3be67c3cc3ef0fc7
Hector <hector.brionesc@edu.mx>
2025-08-27 09:29:15 -0600

    Laboratorio 4

--- Laboratorio 4.R
+++ Laboratorio 4.R
@@ -8,3 +8,33 @@
 calidad <- read.csv("Calidad_plantas.csv", header = T)
 
 calidad$Tratamiento <- as.factor(calidad$Tratamiento)
+
+colores <- c("navajowhite", "skyblue")
+boxplot(calidad$IE ~ calidad$Tratamiento,
+        col = colores,
+        xlab = "Tratamientos",
+        ylab = "indice calidad",
+        ylim = c(0.4,1.2),
+        main = "Vivero Iturbide")
+
+#Estadistica descriptivas
+# tapply sirve para obtener un valor cuando contamos
+# con varios grupos
+
+tapply(calidad$IE, calidad$Tratamiento, mean)
+tapply(calidad$IE, calidad$Tratamiento, var)        
+
+# Observamos que la varianza del grupo fert es 3 veces
+# mas grande que el grupo Control (Ctrl)
+
+# revisar el comportamiento de los datos
+library("ggplot2")
+
+ggplot(calidad, aes(x = IE, color = Tratamiento))+
+  geom_density()
+
+
+
+
+
+
