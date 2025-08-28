5b7637e91ee8438c1c46debdbe0dc0afa0579d1b
Hector <hector.brionesc@edu.mx>
2025-08-28 09:27:43 -0600

    Laboratorio 4

--- Laboratorio 4.R
+++ Laboratorio 4.R
@@ -8,7 +8,7 @@
 calidad <- read.csv("Calidad_plantas.csv", header = T)
 
 calidad$Tratamiento <- as.factor(calidad$Tratamiento)
-
+View(calidad)
 colores <- c("navajowhite", "skyblue")
 boxplot(calidad$IE ~ calidad$Tratamiento,
         col = colores,
@@ -23,6 +23,7 @@ boxplot(calidad$IE ~ calidad$Tratamiento,
 
 tapply(calidad$IE, calidad$Tratamiento, mean)
 tapply(calidad$IE, calidad$Tratamiento, var)        
+tapply(calidad$IE, calidad$Tratamiento, sd)
 
 # Observamos que la varianza del grupo fert es 3 veces
 # mas grande que el grupo Control (Ctrl)
@@ -31,7 +32,50 @@ tapply(calidad$IE, calidad$Tratamiento, var)
 library("ggplot2")
 
 ggplot(calidad, aes(x = IE, color = Tratamiento))+
-  geom_density()
+  geom_density() 
+
+# Separar los datos por tratamiento usando subset
+
+df_ctrl <- subset(calidad, Tratamiento == "Ctrl")
+df_fert <- subset(calidad, Tratamiento != "Ctrl")
+
+# qqnorm revisar normalidad
+par(mfrow = c(1,2))
+qqnorm(df_ctrl$IE); qqline(df_ctrl$IE)
+qqnorm(df_fert$IE); qqline(df_fert$IE)
+par(mfrow = c(1,1))
+
+# Prueba de normalidad
+
+shapiro.test(df_ctrl$IE)
+shapiro.test(df_fert$IE)
+
+
+# Revisar homogeneidad de carianza
+var.test(df_ctrl$IE, df_fert$IE)
+var.test(calidad$IE ~ calidad$Tratamiento)
+
+# Aplicar la prueba t, varianzas iguales
+# Dos colas = two.sided
+
+t.test(calidad$IE ~ calidad$Tratamiento,
+       var.equal = T,
+       alternative = "two.sided")
+
+
+
+cohens_efecto <- function(x,y) {
+  n1 <- length(x); n2 <- length(y)
+  s1 <- sd(x); s2 <-sd(y)
+  sp <- sqrt(((n1 - 1) * s1^2 +(n2 - 1) * s2^2) / (n1 + n2 - 2))
+  (mean(x) - mean(y)) / sp
+  }
+
+d_cal <- cohens_efecto(df_ctrl$IE, df_fert$IE)
+d_cal
+  
+
+
 
 
 
