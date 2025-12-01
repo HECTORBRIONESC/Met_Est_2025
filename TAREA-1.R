437fcd841ce3eeeb9bd688bb0c731e31369a3e41
Hector <hector.brionesc@uanl.edu.mx>
2025-12-01 12:51:15 -0600

    T1

---
+++ TAREA-1.R
@@ -0,0 +1,49 @@
+# gastos totales 
+300 + 240 + 1527 + 400 + 1500 + 1833 
+celular <- 300
+transporte <- 240
+comestibles <- 1527
+gimnasio <- 400
+alquiler <- 1500
+otros <- 1833
+total <- celular+transporte+comestibles+gimnasio+alquiler+otros
+semestre <- total*5
+anual <- total*10
+
+#Valor abtsoluto
+abs(10)
+abs(-4)
+
+#Raiz cuadrada
+sqrt(9)
+
+#Logaritmo natural
+log(2)
+
+2*9
+4+9 # tambien se puede colocar un comentario 
+
+celular <- 300
+Celular <--300
+CELULAR <- 8000
+
+celular+celular
+CELULAR-celular
+help(abs)
+help(mean)
+?abs
+?mean
+help.search("absolute")
+
+gastos <- c(celular,transporte,comestibles,gimnasio,alquiler,otros)
+gastos
+
+barplot(gastos)
+
+sort(gastos)
+gastos_ord <-sort(gastos, decreasing = TRUE)
+barplot(gastos_ord)
+help("barplot")
+barplot(gastos_ord,main = "Gastos mensuales")
+names.arg	= c("otros", "comestibles", "alquiler", "gimnasio", "celular", "transporte")
+
